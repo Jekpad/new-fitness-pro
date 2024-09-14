5f85edd99cd56598e2d0d00864b43a59d10613bc
DimaGAV <gav82gav@gmail.com>
2024-09-14 14:41:05 +0300

    откат

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -22,20 +22,21 @@ function Main() {
   return (
     <ContentWrapper>
       <Header />
-      <div className="mt-[60px] flex flex-col items-start justify-center w-full">
-        <div className="w-full flex flex-col items-start justify-start gap-[10px]">
-          <h1 className="text-3xl md:text-6xl font-medium leading-[40px] md:leading-[60px] text-left">
+      <div className="mt-[60px] flex flex-col items-center justify-center">
+        <div className="flex items-center justify-center gap-[10px]">
+          <h1 className="text-6xl font-medium leading-[60px]">
             Начните заниматься спортом и улучшите качество жизни
           </h1>
+          <div className="flex gap-[10px] bg-[#BCEC30] px-[16px] py-[20px] text-[32px] font-normal leading-[35px]">
+            Измени своё тело за полгода!
+          </div>
         </div>
-        <div className="mt-[30px] md:mt-[50px] flex flex-wrap justify-start gap-[20px] w-full">
+        <div className="mt-[50px] flex flex-wrap justify-start gap-[40px]">
           {courses.map((course) => (
             <Card key={course._id} course={course} uid={user?.uid} initialSubscribed={false} />
           ))}
         </div>
-        <div className="mt-[20px] md:mt-[40px] rounded-[46px] bg-[#BCEC30] px-[26px] py-[16px] cursor-pointer ml-auto">
-          Наверх ↑
-        </div>
+        <div className="rounded-[46px] bg-[#BCEC30] px-[26px] py-[16px]">Наверх ↑</div>
       </div>
     </ContentWrapper>
   );
