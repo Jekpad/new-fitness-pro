48a47cc17ab93837e410a329e893d414921862be
DimaGAV <gav82gav@gmail.com>
2024-09-14 15:43:35 +0300

    +Наверх

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -19,24 +19,36 @@ function Main() {
     fetchCourses();
   }, []);
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <ContentWrapper>
       <Header />
-      <div className="mt-[60px] flex flex-col items-center justify-center">
-        <div className="flex items-center justify-center gap-[10px]">
-          <h1 className="text-6xl font-medium leading-[60px]">
+      <div className="mt-[60px] flex flex-col items-center justify-center md:mt-[25px]">
+        <div className="flex flex-col items-start justify-center gap-[10px] md:flex-row">
+          <h1 className="text-left text-3xl font-medium leading-[35px] md:text-left md:text-6xl md:leading-[60px]">
             Начните заниматься спортом и улучшите качество жизни
           </h1>
-          <div className="flex gap-[10px] bg-[#BCEC30] px-[16px] py-[20px] text-[32px] font-normal leading-[35px]">
+          <div className="hidden gap-[10px] bg-[#BCEC30] px-[16px] py-[20px] text-[32px] font-normal leading-[35px] md:flex">
             Измени своё тело за полгода!
           </div>
         </div>
-        <div className="mt-[50px] flex flex-wrap justify-start gap-[40px]">
+        <div className="mt-[50px] flex w-full flex-wrap justify-start gap-[40px]">
           {courses.map((course) => (
             <Card key={course._id} course={course} uid={user?.uid} initialSubscribed={false} />
           ))}
         </div>
-        <div className="rounded-[46px] bg-[#BCEC30] px-[26px] py-[16px]">Наверх ↑</div>
+        <div 
+          className="ml-auto mt-[20px] cursor-pointer rounded-[46px] bg-[#BCEC30] px-[26px] py-[16px] md:mt-[40px] md:mx-auto"
+          onClick={scrollToTop}
+        >
+          Наверх ↑
+        </div>
       </div>
     </ContentWrapper>
   );
