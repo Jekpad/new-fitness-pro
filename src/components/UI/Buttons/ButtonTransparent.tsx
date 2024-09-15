50dbb395510233f4047001b3ce3cad7163e78209
DimaGAV <gav82gav@gmail.com>
2024-09-15 21:16:08 +0300

    -полужирный из кнопок

--- src/components/UI/Buttons/ButtonTransparent.tsx
+++ src/components/UI/Buttons/ButtonTransparent.tsx
@@ -6,7 +6,7 @@ type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 const ButtonTransparent = ({ children, className, ...props }: Props) => {
   return (
     <button
-      className={`font-semibold bg-color-component-background border-black border-2 hover:bg-color-inactive active:bg-[#E9ECED] px-[26px] py-4 rounded-full ${className}`}
+      className={`bg-color-component-background border-black border-2 hover:bg-color-inactive active:bg-[#E9ECED] px-[26px] py-4 rounded-full ${className}`}
       {...props}
     >
       {children}
