5e3d4697b00ac1ee0fce9a9bb2ea1ba623481ef7
J[A]shy <jekpadt@gmail.com>
2024-09-15 16:58:36 +0300

    done profile page adaptive

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -81,20 +81,22 @@ function Profile() {
         <div className="mt-[50px]">
           <h2 className="text-start text-[40px] font-semibold">Профиль</h2>
         </div>
-        <div className="mt-4 flex gap-[33px] rounded-2xl bg-color-component-background p-10 shadow-lg">
-          <img src="/ProfilePicture.png" alt="Картинка" width={197} height={197} />
+        <div className="mt-4 flex flex-wrap justify-center gap-[33px] rounded-2xl bg-color-component-background p-10 shadow-lg md:justify-start">
+          <img src="/ProfilePicture.png" className="h-[197px] w-[197px]" alt="Фото профиля" />
           <div className="flex flex-col gap-[30px]">
             <p className="text-[32px]">{user?.name}</p>
             <div>
               <p className="text-[18px]">Логин: {user?.email}</p>
               <p className="text-[18px]">Пароль: ******</p>
             </div>
-            <div className="mt-auto">
-              <ButtonRegular className="min-w-[192px]" onClick={() => setModalChangePasswod(true)}>
+            <div className="mt-auto flex flex-wrap gap-[10px]">
+              <ButtonRegular
+                className="w-full min-w-[192px] md:w-auto"
+                onClick={() => setModalChangePasswod(true)}>
                 Изменить пароль
               </ButtonRegular>
               <ButtonTransparent
-                className="ml-[10px] min-w-[192px]"
+                className="w-full min-w-[192px] md:w-auto"
                 onClick={() => {
                   setUser(null);
                   auth.signOut();
@@ -106,7 +108,7 @@ function Profile() {
         </div>
         <div className="mt-[60px]">
           <h2 className="text-[40px] font-semibold">Мои курсы</h2>
-          <div className="mt-[50px] flex flex-wrap justify-start gap-[40px]">
+          <div className="mt-[50px] flex flex-wrap justify-center gap-[40px] lg:justify-start">
             {courses.map((course, index) => (
               <Card
                 key={index}
