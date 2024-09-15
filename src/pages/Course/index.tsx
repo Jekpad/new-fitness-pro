b274f74e504a4b418119ab7f6007fbf5db337111
DimaGAV <gav82gav@gmail.com>
2024-09-15 17:00:05 +0300

    мужик-

--- src/pages/Course/index.tsx
+++ src/pages/Course/index.tsx
@@ -37,10 +37,6 @@ const Course = () => {
     : undefined;
 
   const auth = () => {
-    // !!!
-    // Тестовая авторизация, потом переписать
-    // !!!
-    // setUser(getUserMockup());
     setDisplayModal("signin");
   };
 
@@ -72,60 +68,61 @@ const Course = () => {
     <ContentWrapper>
       <Header />
       <div
-        className={`flex h-[310px] w-full flex-row justify-between rounded-[20px] px-10 mt-[60px]`}
-        style={{ backgroundColor: courseData.color }}
-      >
-        <div className="mt-10">
+        className={`mt-[60px] flex h-[389px] w-full flex-row justify-between rounded-[20px] px-0 pt-16 md:h-[310px] md:px-10 md:pt-0`}
+        style={{ backgroundColor: courseData.color }}>
+        <div className="mt-10 hidden md:block">
           <p className="leading-16 text-6xl font-medium text-white">{courseData?.nameRU}</p>
         </div>
         <img
-          className="rounded-lg"
+          className="h-[310px] w-auto rounded-lg"
           src={courseData.image}
           alt="course_picture"
-          width="auto"
+          width="343"
           height="310"
         />
       </div>
 
-      <section className="mt-[60px]">
-        <p className="text-4xl font-bold">Подойдет для вас, если:</p>
-        <div className="flex flex-row gap-4 mt-10 items-center flex-wrap justify-center">
+      <section className="mt-10 md:mt-[60px]">
+        <p className="text-2xl font-bold md:text-4xl">Подойдет для вас, если:</p>
+        <div className="justify-space-between mt-6 flex flex-wrap items-center gap-4 md:mt-10 md:flex-row md:flex-nowrap">
           {courseData.fitting &&
             Object.values(courseData.fitting).map((fit, index) => (
               <div
                 key={index}
-                className="h-[141px] w-auto rounded-[20px] bg-black p-[17px] flex flex-row items-center overflow-y-hidden max-w-[431px]"
-              >
+                className="flex h-[141px] w-auto max-w-[431px] flex-row items-center overflow-y-hidden rounded-[20px] bg-black p-[17px]">
                 <div className="flex items-center pr-[25px] text-7xl text-[#BCEC30]">
                   {index + 1}
                 </div>
-                <p className="text-[24px] text-white">{fit}</p>
+                <p className="text-lg text-white md:text-2xl">{fit}</p>
               </div>
             ))}
         </div>
       </section>
 
-      <section className="mt-[60px]">
-        <h1 className=" text-4xl font-bold">Направления</h1>
-        <div className="mt-10 w-full gap-x-[126px] gap-y-8 rounded-[20px] p-[30px] bg-color-acсent grid grid-cols-3">
+      <section className="mt-10 md:mt-[60px]">
+        <h1 className="text-2xl font-bold md:text-4xl">Направления</h1>
+        {/* <div className="mt-6 md:mt-10 grid w-full grid-cols-3 gap-x-[126px] gap-y-8 rounded-[20px] bg-color-acсent p-[30px]"> */}
+        {/* <div className="mt-6 md:mt-10 flex md:flex-row md:flex-wrap flex-wrap w-full gap-x-[126px] gap-y-8 rounded-[20px] bg-color-acсent p-[30px]"> */}
+        <div className="mt-6 flex w-full flex-wrap gap-x-[126px] gap-y-8 rounded-[20px] bg-color-acсent p-[30px] md:mt-10 md:grid md:grid-cols-3">
           {courseData.directions &&
             Object.values(courseData.directions).map((direction, index) => (
-              <div key={index} className="flex flex-row gap-2 items-center">
-                <SolidStar />
-                <p className="text-2xl text-nowrap">{direction}</p>
+              <div key={index} className="flex flex-row items-center gap-2">
+                <SolidStar className="h-6 w-6" />
+                <p className="text-nowrap text-lg md:text-2xl">{direction}</p>
               </div>
             ))}
         </div>
       </section>
 
-      <div className="mt-[102px] flex h-[486px] w-full flex-row rounded-[20px] bg-color-component-background shadow-xl relative justify-between">
+      {/* <div className="relative mt-[102px] flex h-[486px] w-full flex-column flex-wrap md:flex-row md:justify-between rounded-[20px] bg-color-component-background shadow-xl">
         <div>
-          <div className="h-[406px] w-[437px] py-[28px] pl-[28px]">
-            <h1 className="h-[120px] w-[398px] text-5xl font-semibold">
+          <div className="order-2 md:order-1">
+          <div className="h-[406px] w-[437px] px-[28px] py-[28px]">
+            <h1 className="h-[120px] w-[398px] text-[32px] font-semibold md:text-5xl">
               Начните путь <br />к новому телу
             </h1>
             <div className="">
-              <ul className="h-[178px] w-[437px] list-disc pb-[28px] pl-[28px] text-2xl/loose">
+              <ul className="h-[178px] w-[437px] list-disc pb-[28px] pl-[28px] text-lg md:text-2xl/loose">
                 <li>проработка всех групп мышц</li>
                 <li>тренировка суставов</li>
                 <li>улучшение циркуляции крови</li>
@@ -134,27 +131,29 @@ const Course = () => {
               </ul>
             </div>
           </div>
-          <div className="cursor-pointer pl-[28px]">
+          <div className="cursor-pointer pl-[24px] md:pl-[28px]">
             {!user?.uid && (
-              <ButtonRegular className="w-full" onClick={auth}>
+              <ButtonRegular className="w-[283px] md:w-full" onClick={auth}>
                 Войдите, чтобы добавить курс
               </ButtonRegular>
             )}
             {user?.uid && !courseSubscribed && (
-              <ButtonRegular className="w-full" onClick={subscribeCourse}>
+              <ButtonRegular className="w-[283px] md:w-full" onClick={subscribeCourse}>
                 Добавить курс
               </ButtonRegular>
             )}
             {user?.uid && courseSubscribed && (
-              <ButtonRegular className="w-full">Вы успешно подписались на курс</ButtonRegular>
+              <ButtonRegular className="w-[283px] md:w-full">
+                Вы успешно подписались на курс
+              </ButtonRegular>
             )}
           </div>
         </div>
-
-        <div className="w-[660px] h-full relative">
-          <div className="w-[660px] h-full overflow-hidden relative">
+        </div>
+        <div className="relative h-full w-[475px] md:w-[660px]">
+          <div className="relative h-full w-[660px] overflow-hidden">
             <img
-              className="rotate-[355deg] pt-[100px] absolute right-[10px]"
+              className="absolute right-[10px] rotate-[355deg] pt-[100px]"
               alt=""
               src="/green_line.png"
               width="670"
@@ -169,14 +168,79 @@ const Course = () => {
             height="539"
           />
           <img
-            className="absolute top-5 right-96"
+            className="absolute right-96 top-5"
             alt=""
             src="/black_line.png"
             width="50"
             height="42"
           />
         </div>
+      </div> */}
+      <div className="relative mt-[0] md:mt-[102px] flex h-[486px] w-full flex-col md:flex-row md:justify-between rounded-[20px] bg-color-component-background shadow-xl">
+  {/* Блок с картинками */}
+  <div className="order-1 md:order-2 relative h-full w-[475px] md:w-[660px] left-9 md:left-0">
+    <div className="relative h-full w-full overflow-hidden">
+      <img
+        className="absolute right-[10px] rotate-[355deg] pt-[100px]"
+        alt="green line"
+        src="/green_line.png"
+        width="670"
+        height="390"
+      />
+    </div>
+    <img
+      className="absolute md:bottom-5 md:right-2 bottom-[80px] right-[20px]"
+      alt="running man"
+      src="/running_man.png"
+      md-width="519"
+      width="334"
+      height="539"
+    />
+    <img
+      className="absolute right-[340px] md:right-96 md:top-5 top-[0]"
+      alt="black line"
+      src="/black_line.png"
+      width="50"
+      height="42"
+    />
+  </div>
+
+  {/* Блок со списком */}
+  <div className="order-2 md:order-1 absolute z-10 left-10 top-10 md:static md:z-auto md:left-0 md:top-0">
+    <div className="h-[406px] w-[437px] px-[28px] py-[28px] bg-white">
+      <h1 className="h-[120px] w-[398px] text-[32px] font-semibold md:text-5xl">
+        Начните путь <br />к новому телу
+      </h1>
+      <div>
+        <ul className="h-[178px] w-[437px] list-disc pb-[28px] pl-[28px] text-lg md:text-2xl leading-loose">
+          <li>проработка всех групп мышц</li>
+          <li>тренировка суставов</li>
+          <li>улучшение циркуляции крови</li>
+          <li>упражнения заряжают бодростью</li>
+          <li>помогают противостоять стрессам</li>
+        </ul>
       </div>
+    </div>
+    <div className="cursor-pointer pl-[24px] md:pl-[28px]">
+      {!user?.uid && (
+        <ButtonRegular className="w-[283px] md:w-full" onClick={auth}>
+          Войдите, чтобы добавить курс
+        </ButtonRegular>
+      )}
+      {user?.uid && !courseSubscribed && (
+        <ButtonRegular className="w-[283px] md:w-full" onClick={subscribeCourse}>
+          Добавить курс
+        </ButtonRegular>
+      )}
+      {user?.uid && courseSubscribed && (
+        <ButtonRegular className="w-[283px] md:w-full">
+          Вы успешно подписались на курс
+        </ButtonRegular>
+      )}
+    </div>
+  </div>
+</div>
+
       {displayModal === "signin" && <ModalSignIn setDisplayModal={setDisplayModal} />}
       {displayModal === "signup" && <ModalSignUp setDisplayModal={setDisplayModal} />}
     </ContentWrapper>
